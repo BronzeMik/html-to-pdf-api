a478fee3bd35776f798e85d8f00fc7455bd73566
Mikaila Brown <mbr2201@wgu.edu>
2024-04-21 17:20:04 -0400

    vercel

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 
-import Application from "./application.js";
+import Application from "../application.js";
 import express from 'express';
 
 const app = express();
-const port = 3000;
+
 
 
 app.use(express.json())
