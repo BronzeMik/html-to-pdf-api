d711934b64b30a00f97efe68110c6c26a5fb0b80
Mikaila Brown <mbr2201@wgu.edu>
2024-04-21 18:44:19 -0400

    update cors

--- src/application.js
+++ src/application.js
@@ -3,13 +3,18 @@ import conversionController from "./controllers/conversion.js";
 import cors from 'cors';
 
 const port = 3000;
-const corsOptions = {
-    origin: '*'
-}
-class Application {
 
+class Application {
+    constructor(){
+        this.corsOptions = {
+        origin: '*',
+        optionsSuccessStatus: 200 
+    }  
+     
+}
+   
     useMiddleWare() {
-        this.app.use(cors(corsOptions))
+        this.app.use(cors(this.corsOptions))
         this.app.use(express.json());
         this.app.use('/conversion', conversionController)
     }
